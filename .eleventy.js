bf53d7cca12bac47b76a985b2768a997f7729823
Joris Goovaerts <joris.goovaerts@gmail.com>
2020-04-22 13:52:01 +0200

    Add custom anchors

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
+  const markdownIt = require("markdown-it");
+  const markdownItAnchor = require("markdown-it-anchor");
+  const markdownItAttrs = require("markdown-it-attrs");
+  const options = {
+    html: true
+  };
+  let markdownLib = markdownIt(options).use(markdownItAttrs, markdownItAnchor);
+  
+  eleventyConfig.setLibrary("md", markdownLib);
+
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy('css')
   
