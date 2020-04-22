d7d1f7770e87f6501bdafdf430b52988f5055110
Joris Goovaerts <joris.goovaerts@gmail.com>
2020-04-22 14:37:12 +0200

    Cleanup location required statements

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItAttrs = require("markdown-it-attrs");
 
 module.exports = function(eleventyConfig) {
-  const markdownIt = require("markdown-it");
-  const markdownItAnchor = require("markdown-it-anchor");
-  const markdownItAttrs = require("markdown-it-attrs");
   const options = {
     html: true
   };
-  let markdownLib = markdownIt(options).use(markdownItAttrs, markdownItAnchor);
+
+  const markdownLib = markdownIt(options).use(markdownItAttrs, markdownItAnchor);
   
   eleventyConfig.setLibrary("md", markdownLib);
 
