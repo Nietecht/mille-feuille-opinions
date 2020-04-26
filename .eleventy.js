5704519687d0d7445cf71f4c3403ea1fa9dbaaed
Joris Goovaerts <joris.goovaerts@gmail.com>
2020-04-26 15:09:04 +0200

    Add abbreviation support

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,15 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const markdownItAttrs = require("markdown-it-attrs");
+const markdownItAbbr = require("markdown-it-abbr");
 
 module.exports = function(eleventyConfig) {
   const options = {
     html: true
   };
 
-  const markdownLib = markdownIt(options).use(markdownItAttrs, markdownItAnchor);
+  const markdownLib = markdownIt(options).use(markdownItAttrs, markdownItAnchor)
+  .use(markdownItAbbr);
   
   eleventyConfig.setLibrary("md", markdownLib);
 
